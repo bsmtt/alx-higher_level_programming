be47835ccb06bf44c33438e88ec3edd8d7e83e66
bsmtt <basmatalaat224@gmail.com>
2024-02-06 14:36:41 -0800

    feat: position

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -73,6 +73,7 @@ class Square:
             raise TypeError("position must be a tuple of 2 positive integers")
         if value[0] < 0 or value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
 
     def my_print(self):
         """prints the square with the character #
