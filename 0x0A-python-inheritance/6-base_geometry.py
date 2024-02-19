0cda209bc27683c93a498840d089e308599d4923
bsmtt <basmatalaat224@gmail.com>
2024-02-19 15:29:13 -0800

    feat: base_geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+'''
+    Module for BaseGeometry class.
+'''
+
+
+class BaseGeometry:
+    ''' A BaseGeometry class. '''
+
+    @classmethod
+    def area(self):
+        '''Method to compute this area.'''
+        raise Exception('area() is not implemented')
