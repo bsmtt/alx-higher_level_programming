e47bbd9af75d7e80f78f68802412310560166669
bsmtt <basmatalaat224@gmail.com>
2024-02-19 15:15:17 -0800

    feat: 4-inherits_from.py

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''
+    Module of inherits_from method.
+'''
+
+
+
+def inherits_from(obj, a_class):
+    ''' subclass of a class. '''
+    return isinstance(obj, a_class) and type(obj) != a_class
