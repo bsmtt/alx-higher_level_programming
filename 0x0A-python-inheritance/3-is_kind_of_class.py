50b0a418a997ee7e9cd1b686715558f50d21c95d
bsmtt <basmatalaat224@gmail.com>
2024-02-19 14:46:29 -0800

    feat: kind_of_class

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''
+    Module of is_kind_of_class
+'''
+
+
+def is_kind_of_class(obj, a_class):
+    ''' Is object is a subclass of a class. '''
+
+    return isinstance(obj, a_class)
