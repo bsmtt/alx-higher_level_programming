4b736796e00355afad5adfbdfa1b0141f2b96b06
bsmtt <basmatalaat224@gmail.com>
2024-02-20 12:56:43 -0800

    feat: write_file

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import json
+"""
+    Fro JSON Module
+"""
+
+
+def from_json_string(my_str):
+    """returns the Object represented by an JSON"""
+    return json.loads(my_str)
