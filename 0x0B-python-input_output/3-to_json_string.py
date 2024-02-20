d13d59006e539a5b57977266fe7671d4dca7caec
bsmtt <basmatalaat224@gmail.com>
2024-02-20 12:53:27 -0800

    feat: write_file

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+    To Json Module
+"""
+
+
+import json
+
+
+def to_json_string(my_obj):
+    """returns he JSON representation of an object"""
+    return json.dumps(my_obj)
