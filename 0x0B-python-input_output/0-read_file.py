e7edc99587c47ec549e2714584d1eeaf2d850dc7
bsmtt <basmatalaat224@gmail.com>
2024-02-20 12:42:45 -0800

    feat: read_file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
+"""
+    Read File Module
+"""
+
 
 def read_file(filename=""):
     """reads a text file and prints it"""
 
-    with open(filename) as f
+    with open(filename) as f:
         read_file = f.read()
         print(read_file, end="")
