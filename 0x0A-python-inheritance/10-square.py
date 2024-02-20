f9d5f3580a4e74e671781623d58d485dc1be0de3
bsmtt <basmatalaat224@gmail.com>
2024-02-19 16:39:11 -0800

    feat: square

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+Rectangle = __import__('9-rectangle').Rectangle
+"""
+module with class BaseGeometry
+
+"""
+
+class Square(Rectangle):
+    """Square class that inherits from Rectangle that inherits BaseGeometry"""
+
+    def __init__(self, size)
+        '''Constructor.'''
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def ara (size):
+        return size ** 2
