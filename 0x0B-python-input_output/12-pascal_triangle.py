53e178f876352c1cd1e6cb25f52437863d2e46e6
bsmtt <basmatalaat224@gmail.com>
2024-02-20 15:39:38 -0800

    feat: pascal_triangle

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
+"""
+    pascal module
+"""
 
-def pascal_triangle(n):
-    ls = []
-    for i in range(1, n + 1):
-        for j in range(1, i + 1):
-            ls.append(j)
-    return ls
+
+if __name__ == "__main__":
+    def pascal_triangle(n):
+        """ pascal triangle"""
+        ls = []
+        for i in range(1, n + 1):
+            for j in range(1, i + 1):
+                ls.append(j)
+        return ls
